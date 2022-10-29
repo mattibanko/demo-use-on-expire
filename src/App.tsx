9c4cb1431fd94eeaae071f218a281229f84ce112
Mateusz Bańka <mbanka@e-gaming.cz>
2022-10-29 13:05:00 +0200

    init part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import { useOnExpire } from "use-on-expire";
+import { format } from "date-fns";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState<Date>(new Date());
+  const [date, setDate] = useState<Date>();
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const timer = useCallback(
+    (): NodeJS.Timeout =>
+      setTimeout(() => {
+        setCurrentTime(new Date());
+        return timer();
+      }, 1000),
+    []
+  );
+
+  const { timeoutId, dateToRefresh } = useOnExpire({
+    date,
+    fn: useCallback(() => setSuccess(true), []),
+  });
+  const minDate = currentTime.toISOString().split(":");
+
+  useEffect(() => {
+    const timeout = timer();
+
+    return () => clearTimeout(timeout);
+  }, [timeoutId, success, timer]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-slate-300	flex flex-col h-screen w-screen items-center justify-center">
+      <div className="rounded bg-slate-200 shadow-lg shadow-gray-400 border-2 p-4	border-slate-100	 flex flex-col content-center justify-center min-[1220px]:w-1/2 max-[1000px]:w-full gap-10">
+        <div className="text-xl text-gray-700">
+          Current time: {format(currentTime, "yyyy-MM-dd hh:mm:ss")}
+        </div>
+        <div className="text-xl text-gray-700">
+          Fire on:{" "}
+          {(dateToRefresh && format(dateToRefresh, "yyyy-MM-dd hh:mm:ss")) ??
+            "Please choose date and wait..."}
+        </div>
+        {timeoutId && !success && (
+          <div className="text-3xl text-gree-500">Wait for it...</div>
+        )}
+        {success && (
+          <div className="text-3xl text-pink-500">
+            Congratulations, your function has been fired!
+          </div>
+        )}
+        <input
+          type="datetime-local"
+          min={`${minDate[0]}:${minDate[1]}`}
+          step="1"
+          onChange={({ currentTarget: { value } }) => {
+            if (value) {
+              const newDate = new Date(value);
+              if (newDate.getTime() > new Date().getTime()) {
+                console.log(newDate);
+                setDate(new Date(value));
+                setSuccess(false);
+              }
+            }
+          }}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
